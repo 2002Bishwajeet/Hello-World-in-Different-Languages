84969cfdb07fab4e783db38ea44b746999581e10
Bishwajeet Parhi <62933155+2002Bishwajeet@users.noreply.github.com>
2021-03-29 14:20:23 +0530

    hello world

---
+++ C#/HelloWorld.cs
@@ -0,0 +1,18 @@
+
+using System;
+  
+
+namespace HelloWorldApp {
+
+    class Hello {
+
+        static void Main(string[] args) {
+              
+          
+            Console.WriteLine("Hello World!");
+              
+            
+            Console.ReadKey();
+        }
+    }
+}
